b36b43e472b78e0e2f12a80393c1547aa82be7d8
Chimey Palyul <palyul.chimey@gmail.com>
2024-02-06 16:40:37 -0700

    need to do the ruby one

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,5 +1,9 @@
 function reverseString(str) {
-  // type your code here
+  let reverse = ''
+  for(let i = str.length -1; i > -1; --i){
+    reverse = reverse + str[i]
+  }
+  return reverse;
 }
 
 if (require.main === module) {
@@ -16,4 +20,12 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+//So the first thing I need to do is create a string (which i called reverse) that will hold the string in reverse. I first set this to an empty string. 
+//Next, I need to iterate through the string, using the for loop, and an iterator(i). The iterator (i) will start at index -1 indicating that it will begin at the last letter.
+//the i>-1 will indicate that it needs to iterate for as long as the counter is greater than -1, effectively stopping iteration at 0 or the first element in the string.
+// the --i will indicate what needs to be done after each iteration
+// we then need to append the results of our function to the empty reverse string, thereby storing it. 
+// we then need to return the 'reverse' string 
+
+
 // And a written explanation of your solution
